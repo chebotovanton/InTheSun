6abc4319d1763ecd6d980c4ce06ef7fcedf23a31
Anton <anton.chebotov@aviasales.ru>
2016-03-07 10:36:13 +0300

    Empty screens added

--- InTheSun/AMTabMenuVC.m
+++ InTheSun/AMTabMenuVC.m
@@ -1,12 +1,17 @@
 #import "AMTabMenuVC.h"
 #import "InTheSun-Swift.h"
 #import "SecondViewController.h"
+#import "AMAlbumInfoVC.h"
+#import "EventsVC.h"
 
 @implementation AMTabMenuVC
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    self.tabBar.tintColor = [UIColor yellowColor];
+    self.tabBar.backgroundColor = [UIColor colorWithRed:44.0/255.0 green:44.0/255.0 blue:44.0/255.0 alpha:1.0];
+    self.tabBar.barStyle = UIBarStyleBlack;
 
     NSArray *vcs = [self createControllers];
     [self setViewControllers:vcs animated:YES];
@@ -20,6 +25,17 @@
     musicBarItem.title = @"Music";
     musicVC.tabBarItem = musicBarItem;
 
+    AMAlbumInfoVC *albumInfoVC = [[AMAlbumInfoVC alloc] initWithNibName:@"AMAlbumInfoVC" bundle:nil];
+    UITabBarItem * albumBarItem = [[UITabBarItem alloc] initWithTitle:nil image:[UIImage imageNamed:@"musicMenuIcon"] tag:0];
+    albumBarItem.imageInsets = UIEdgeInsetsMake(6.0, 0.0, -6.0, 0.0);
+    albumBarItem.title = @"Album Info";
+    albumInfoVC.tabBarItem = albumBarItem;
+    
+    EventsVC *eventsVC = [[EventsVC alloc] initWithNibName:@"EventsVC" bundle:nil];
+    UITabBarItem * eventsBarItem = [[UITabBarItem alloc] initWithTitle:nil image:[UIImage imageNamed:@"musicMenuIcon"] tag:0];
+    eventsBarItem.imageInsets = UIEdgeInsetsMake(6.0, 0.0, -6.0, 0.0);
+    eventsBarItem.title = @"Events";
+    eventsVC.tabBarItem = eventsBarItem;
 
     SecondViewController *secondVC = [[SecondViewController alloc] initWithNibName:@"SecondViewController" bundle:nil];
     UITabBarItem * secondBarItem = [[UITabBarItem alloc] initWithTitle:nil image:[UIImage imageNamed:@"secondMenuIcon"] tag:0];
@@ -27,7 +43,7 @@
     secondBarItem.title = @"Second";
     secondVC.tabBarItem = secondBarItem;
     
-    return @[musicVC, secondVC];
+    return @[musicVC, albumInfoVC, eventsVC, secondVC];
 }
 
 @end
---
+++ InTheSun/Common/Events/EventsVC.m
@@ -0,0 +1,40 @@
+#import "EventsVC.h"
+
+@interface EventsVC () <UITableViewDataSource, UITableViewDelegate>
+
+@property (nonatomic, strong) NSArray *items;
+
+@end
+
+@implementation EventsVC
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+}
+
+#pragma mark - UITableViewDataSource
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return self.items.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    return [UITableViewCell new];
+}
+
+#pragma mark - UITableViewDelegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
+@end
