1bd1aaaaeb1768708ca0329d068321fa7f96fd65
Anton <anton.chebotov@aviasales.ru>
2016-03-26 20:19:56 +0300

    OpenCV as pod

---
+++ InTheSun/ImageProcessor/OpenCVWrapper.h
@@ -0,0 +1,8 @@
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface OpenCVWrapper : NSObject
+
++ (BOOL) imageHasCircle:(UIImage*)image;
+
+@end
---
+++ InTheSun/ImageProcessor/OpenCVWrapper.mm
@@ -0,0 +1,30 @@
+#import "OpenCVWrapper.h"
+#import "UIImage+OpenCV.h"
+
+@implementation OpenCVWrapper
+
++ (BOOL) imageHasCircle:(UIImage*)image {
+    
+    cv::Mat imageMatrix = [image CVMat];
+    
+    if(imageMatrix.empty()) {
+        return NO;
+    }
+    
+    cv::Mat cimg;
+    if ( imageMatrix.type()==CV_8UC1 ) {
+        cvtColor(imageMatrix, cimg, CV_GRAY2RGB);
+    } else {
+        cimg = imageMatrix;
+        cvtColor(imageMatrix, imageMatrix, CV_RGB2GRAY);
+    }
+    medianBlur(imageMatrix, imageMatrix, 5);
+    
+    std::vector<cv::Vec3f> circles;
+    // We need to have more tests here ;)
+    HoughCircles(imageMatrix, circles, CV_HOUGH_GRADIENT, 1, 1000, 100, 30, 150, 400);
+    
+    return circles.size() > 0;
+}
+
+@end
