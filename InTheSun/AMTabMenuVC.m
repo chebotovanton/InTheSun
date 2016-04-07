93ab4088c2de50f749ed524ea923751e98e0d4db
Anton <anton.chebotov@aviasales.ru>
2016-04-07 20:36:20 +0300

    Tab bar titles offset

--- InTheSun/AMTabMenuVC.m
+++ InTheSun/AMTabMenuVC.m
@@ -26,18 +26,23 @@
 
 - (NSArray *)createControllers
 {
+    UIOffset titleOffset = UIOffsetMake(0.0, -3.0);
+    
     AMMusicViewController *musicVC = [[AMMusicViewController alloc] initWithNibName:@"AMMusicViewController" bundle:nil];
-    UITabBarItem * musicBarItem = [[UITabBarItem alloc] initWithTitle:nil image:[UIImage imageNamed:@"musicMenuIcon"] tag:0];
+    UITabBarItem *musicBarItem = [[UITabBarItem alloc] initWithTitle:nil image:[UIImage imageNamed:@"musicMenuIcon"] tag:0];
+    musicBarItem.titlePositionAdjustment = titleOffset;
     musicBarItem.title = LS(@"LOC_TAB_BAR_MUSIC");
     musicVC.tabBarItem = musicBarItem;
 
     AMAlbumInfoVC *albumInfoVC = [[AMAlbumInfoVC alloc] initWithNibName:@"AMAlbumInfoVC" bundle:nil];
-    UITabBarItem * albumBarItem = [[UITabBarItem alloc] initWithTitle:nil image:[UIImage imageNamed:@"albumMenuIcon"] tag:0];
+    UITabBarItem *albumBarItem = [[UITabBarItem alloc] initWithTitle:nil image:[UIImage imageNamed:@"albumMenuIcon"] tag:0];
+    albumBarItem.titlePositionAdjustment = titleOffset;
     albumBarItem.title = LS(@"LOC_TAB_BAR_ALBUM");
     albumInfoVC.tabBarItem = albumBarItem;
     
     EventsVC *eventsVC = [[EventsVC alloc] initWithNibName:@"EventsVC" bundle:nil];
-    UITabBarItem * eventsBarItem = [[UITabBarItem alloc] initWithTitle:nil image:[UIImage imageNamed:@"eventsMenuIcon"] tag:0];
+    UITabBarItem *eventsBarItem = [[UITabBarItem alloc] initWithTitle:nil image:[UIImage imageNamed:@"eventsMenuIcon"] tag:0];
+    eventsBarItem.titlePositionAdjustment = titleOffset;
     eventsBarItem.title = LS(@"LOC_TAB_BAR_EVENTS");
     eventsVC.tabBarItem = eventsBarItem;
     
