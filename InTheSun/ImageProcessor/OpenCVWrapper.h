9738e2c3060a949ee94761c9dd731342c11f063c
Anton <anton.chebotov@aviasales.ru>
2016-04-10 14:45:55 +0300

    OpenCV references removed

--- InTheSun/ImageProcessor/OpenCVWrapper.h
+++
@@ -1,9 +0,0 @@
-#import <Foundation/Foundation.h>
-#import <UIKit/UIKit.h>
-
-@interface OpenCVWrapper : NSObject
-
-+ (BOOL)imageHasCircle:(UIImage *)image;
-+ (CGPoint)circleCenterAtImage:(UIImage *)image;
-
-@end
